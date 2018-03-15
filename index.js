b6682114b5b9fa127a5ea5796160f1e7ae1dcd89
Joe Previte <jjprevite@gmail.com>
2018-03-14 21:32:59 -0700

    Add function to getUserById, count beers given. TODO: figure out how to find users by id.

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
+require('dotenv').config();
 const { RTMClient, WebClient } = require('@slack/client');
 const token = process.env.SLACK_BOT_TOKEN;
-
+const web = new WebClient(token);
 const rtm = new RTMClient(token);
+
 rtm.start();
 
-const web = new WebClient(token);
+rtm.on('message', (message) => {
+  if (message.text.includes(':beer:')) {
+    let beerGiverUserId = message.user;
+    let numberOfBeers = message.text.match(/:beer:/g).length;
+    let mentionedUsers = getUsers(message.text);
+    let beerGiver = findUserById(beerGiverUserId);
+    let beerReceivers = mentionedUsers.map((u) => {
+      return findUserById(u)
+    })
+    console.log(`(channel:${message.channel}) ${beerGiver} gave ${numberOfBeers} to ${beerReceivers}!`);
+  }
 
-web.channels.list().then((res) => {
-  const channels = res.channels.filter(c => c.is_member);
-  console.log(channels);
-  if (channels.length > 0) {
-    for (let channel of channels) {
-      rtm.sendMessage('Hey Joe, look at me...', channel.id).then((msg) => {
-        console.log(`Message sent to channel ${channel.name} with ts: ${msg.ts}`);
-      }).catch(console.error);
-    }
+  // Skip messages that are from a bot or my own user ID
+  if ((message.subtype && message.subtype === 'bot_message') ||
+    (!message.subtype && message.user === rtm.activeUserId)) {
+    return;
   }
-  else {
-    console.log('sucks to suck');
-  }
+
+  // Log the message
+  console.log(`(channel:${message.channel}) ${message.user} says: ${message.text}`);
 });
+
+
+function getUsers(text) {
+  let userTags = text.match(/\<@U\w+>/g);
+  return userTags.map((user) => {
+    return user.replace(/[<@>]/g, '');
+  });
+}
+
+function findUserById(id) {
+  let u;
+  web.users.list()
+    .then((res) => {
+      const user = res.members.find(u => u.id === id);
+
+      if (user) {
+        console.log(user.profile.display_name);
+      } else {
+        console.log('no user to give beer to');
+      }
+    });
+}
