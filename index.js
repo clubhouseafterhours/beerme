27d0bd5a695aacc17f6d28568e11d49ed971c833
Tad Scritchfield <tadscritch@gmail.com>
2018-03-14 19:50:32 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { RTMClient, WebClient } = require('@slack/client');
+const token = 'xoxb-329565682272-2wvvdBa6C5HwdE2VDdXhQH7r';
+
+const rtm = new RTMClient(token);
+rtm.start();
+
+const web = new WebClient(token);
+
+web.channels.list().then((res) => {
+  const channels = res.channels.filter(c => c.is_member);
+  console.log(channels);
+  if (channels.length > 0) {
+    for (let channel of channels) {
+      rtm.sendMessage('Hey Joe, look at me...', channel.id).then((msg) => {
+        console.log(`Message sent to channel ${channel.name} with ts: ${msg.ts}`);
+      }).catch(console.error);
+    }
+  }
+  else {
+    console.log('sucks to suck');
+  }
+});
